41ffc7e4653305a1b1f6eadf1c5369e6222451f9
abelmorad <abelmorad97@gmail.com>
2023-07-09 14:33:37 +0800

    Feat: Hero carousel

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,8 +1,41 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 function Home() {
+  function HeroSlider() {
+    const data = [
+      "https://burst.shopifycdn.com/photos/man-puts-his-hand-inside-his-jacket.jpg?width=1024&format=pjpg&exif=1&iptc=1",
+      "https://burst.shopifycdn.com/photos/casual-man-sits-on-rock.jpg?width=1024&format=pjpg&exif=1&iptc=1",
+      "https://burst.shopifycdn.com/photos/smiling-man-in-formalwear.jpg?width=1024&format=pjpg&exif=1&iptc=1",
+      "https://burst.shopifycdn.com/photos/autumn-fashion-on-man-with-glasses.jpg?width=1024&format=pjpg&exif=1&iptc=1",
+      "https://burst.shopifycdn.com/photos/skateboarder-rides-past.jpg?width=1024&format=pjpg&exif=1&iptc=1"
+    ];
+
+    return (
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        slidesPerView={1}
+        autoplay
+        pagination
+      >
+        {data.map((data) => (
+          <SwiperSlide className="flex place-content-center">
+            <img src={data} alt="mens" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  }
   return (
-    <div className="text-lime-900">Home</div>
-  )
+    <>
+      <section className="flex pt-16 mx-10">
+        <HeroSlider />
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
