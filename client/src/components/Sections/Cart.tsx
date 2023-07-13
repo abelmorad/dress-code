5d218830c1bd372cc6cfccdb8144637f4b1b8681
abelmorad <abelmorad97@gmail.com>
2023-07-13 08:26:59 +0800

    update

--- client/src/components/Sections/Cart.tsx
+++ client/src/components/Sections/Cart.tsx
@@ -33,11 +33,11 @@ function Cart() {
   ];
 
   return (
-    <section className="flex flex-col absolute bg-white mt-3 pb-3 right-0 pl-5 pr-12 -z-10 w-1/3 shadow-2xl overflow-scroll">
-      <h1 className="mb-5">Products in your Cart</h1>
+    <section className="flex flex-col absolute bg-white mt-3 pb-3 right-0 pl-5 pr-12 -z-10 w-1/3 shadow-2xl">
+      <h1 className="my-5">Products in your Cart</h1>
       {cardData?.map((data) => (
         <div key={data.id}>
-          <div className="flex place-content-center items-center">
+          <div className="flex place-content-center items-start justify-between mb-3">
             <img
               className="h-36 w-24 object-cover mr-5"
               src={data.img}
@@ -45,21 +45,21 @@ function Cart() {
             />
             <div className="flex flex-col mr-2">
               <h2 className="mb-5">{data.title}</h2>
-              <p className="mb-2 text-gray-600">{data.desc.substring(0, 60)}</p>
+              <p className="mb-2 text-gray-600">{data.desc.substring(0, 40)}</p>
               <p className="text-blue-500">1 x {data.currentPrice} USD</p>
             </div>
             <DeleteOutlineOutlinedIcon className="text-red-600" />
           </div>
-          <div className="flex justify-between font-medium my-3">
-            <p>SUBTOTAL</p>
-            <p>19 USD</p>
-          </div>
-          <button className="flex place-content-center items-center uppercase bg-blue-500 text-white w-64 py-1 font-medium mb-3">
-            proceed to checkout
-          </button>
-          <p className="text-red-600">Reset Cart</p>
         </div>
       ))}
+      <div className="flex justify-between font-medium my-3">
+        <p>SUBTOTAL</p>
+        <p>19 USD</p>
+      </div>
+      <button className="flex place-content-center items-center uppercase bg-blue-500 text-white w-64 py-1 font-medium mb-3">
+        proceed to checkout
+      </button>
+      <p className="text-red-600">Reset Cart</p>
     </section>
   );
 }
--- client/src/components/Sections/CategorySection.tsx
+++ client/src/components/Sections/CategorySection.tsx
@@ -14,23 +14,23 @@ function CategorySection() {
         <img src={sale} alt="sale" />
         <p className="font-semibold text-white text-6xl absolute mt-60 ml-2">Sale</p>
       </Link>
-      <Link to="/" className="flex area-newseason">
+      <Link to="/products/1" className="flex area-newseason">
       <img style={{height: "640px", width:"310px", objectFit: "cover"}}  src={newseason} alt="newseason" />
         <p className="font-semibold text-white text-6xl absolute ml-24 mt-2 text-right">New <br />Season</p>
       </Link>
-      <Link to="/" className="flex area-men">
+      <Link to="/products/1" className="flex area-men">
       <img src={men} alt="men" />
         <p className="font-semibold text-white text-6xl absolute mt-60 ml-2">Men</p>
       </Link>
-      <Link to="/" className="flex area-accessories">
+      <Link to="/products/1" className="flex area-accessories">
       <img src={accessories} alt="accessories" />
         <p className="font-semibold text-white text-5xl absolute mt-2 ml-2">Accessories</p>
       </Link>
-      <Link to="/" className="flex area-women">
+      <Link to="/products/1" className="flex area-women">
       <img src={women} alt="women" />
         <p className="font-semibold text-white text-6xl absolute mt-60 ml-24">Women</p>
       </Link>
-      <Link to="/" className="flex area-shoes">
+      <Link to="/products/1" className="flex area-shoes">
       <img style={{height: "310px", width:"640px", objectFit: "cover"}} src={shoes} alt="shoes" />
         <p className="font-semibold text-white text-6xl absolute mt-2 ml-2">Shoes</p>
       </Link>
