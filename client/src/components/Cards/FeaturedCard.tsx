0d3ef0721f697301316ca3e7bb2dff209db49560
abelmorad <abelmorad97@gmail.com>
2023-07-14 18:57:26 +0800

    Removed an import

--- client/src/components/Cards/FeaturedCard.tsx
+++ client/src/components/Cards/FeaturedCard.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { features } from "process";
 
 function FeaturedCard() {
   const [data, setData] = useState<any[]>([]);
