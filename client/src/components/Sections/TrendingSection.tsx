92404d5de35732c9337685235d3fa4575a77ba9c
abelmorad <abelmorad97@gmail.com>
2023-07-14 20:14:52 +0800

    Refactor

--- client/src/components/Sections/TrendingSection.tsx
+++ client/src/components/Sections/TrendingSection.tsx
@@ -4,7 +4,7 @@ function TrendingSection() {
   return (
     <section className="flex flex-col">
       <div className="mb-10">
-        <h1 className="text-5xl font-semibold">Featured Products</h1>
+        <h1 className="text-5xl font-semibold">Trending Products</h1>
       </div>
       <div className= "grid grid-flow-col grid-cols-4 grid-row-auto place-content-center gap-8">
         <TrendingCard />
