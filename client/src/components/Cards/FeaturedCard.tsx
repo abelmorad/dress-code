44e4b211dfe36c0b0878c4eeda81f9480798c869
abelmorad <abelmorad97@gmail.com>
2023-07-14 16:27:03 +0800

    Feat: when old price is null it will not display now

--- client/src/components/Cards/FeaturedCard.tsx
+++ client/src/components/Cards/FeaturedCard.tsx
@@ -5,21 +5,6 @@ import axios from "axios";
 function FeaturedCard() {
   const [data, setData] = useState<any[]>([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:1337/api/products", {
-  //       headers: {
-  //         Authorization: "bearer" + process.env.REACT_APP_API_TOKEN,
-  //       },
-  //     })
-  //     .then((res) => {
-  //       setData(res.data.data);
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -58,12 +43,17 @@ function FeaturedCard() {
                 {data.attributes.title}
               </p>
               <div className="flex flex-col">
-                <p className="text-gray-500 line-through">
-                  {data.attributes.oldPrice} <span>USD</span>
+                <p
+                  className="text-gray-500 line-through"
+                  style={
+                    data.attributes.oldPrice === null
+                      ? { display: "none" }
+                      : { display: "block" }
+                  }
+                >
+                  {data.attributes.oldPrice} USD
                 </p>
-                <p className="font-semibold">
-                  {data.attributes.price} <span>USD</span>
-                </p>
+                <p className="font-semibold">{data.attributes.price} USD</p>
               </div>
             </div>
           </div>
@@ -74,37 +64,3 @@ function FeaturedCard() {
 }
 
 export default FeaturedCard;
-
-{
-  /* <Link to={`/product/${data.data.id}`}>
-          <div
-            key={}
-            className="flex flex-col h-full outline-1 outline outline-gray-400"
-          >
-            <img className="w-screen object-cover" src={} alt={} />
-            <p
-              className="absolute px-2 py-2 text-4xl text-black font-semibold italic"
-              style={
-                data.id.attributes.isNew
-                  ? { display: "flex", textShadow: "white 5px 5px" }
-                  : { display: "none" }
-              }
-            >
-              New Season
-            </p>
-            <div className="flex flex-col p-3">
-              <p className="font-semibold text-xl mt-2">
-                {data.id.attributes.title}
-              </p>
-              <div className="flex flex-col">
-                <p className="text-gray-500 line-through">
-                  {data.oldPrice} <span>USD</span>
-                </p>
-                <p className="font-semibold">
-                  {data.id.attributes.price} <span>USD</span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </Link>; */
-}
