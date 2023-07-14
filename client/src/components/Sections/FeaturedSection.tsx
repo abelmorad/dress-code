660bb1fc52a76d228958e7bfcc898213c45a221c
abelmorad <abelmorad97@gmail.com>
2023-07-14 19:05:54 +0800

    Refactor

--- client/src/components/Sections/FeaturedSection.tsx
+++ client/src/components/Sections/FeaturedSection.tsx
@@ -1,10 +1,10 @@
 import FeaturedCard from "../Cards/FeaturedCard";
 
-function FeaturedSection({ sectionTitle }: { sectionTitle: string }) {
+function FeaturedSection() {
   return (
     <section className="flex flex-col">
       <div className="mb-10">
-        <h1 className="text-5xl font-semibold">{sectionTitle}</h1>
+        <h1 className="text-5xl font-semibold">Featured Products</h1>
       </div>
       <div className= "grid grid-flow-col grid-cols-4 grid-row-auto place-content-center gap-8">
         <FeaturedCard />
