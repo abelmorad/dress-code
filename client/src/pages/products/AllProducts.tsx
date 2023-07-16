7eca32066d222354c0dc60765fd5618f7560e79c
abelmorad <abelmorad97@gmail.com>
2023-07-16 18:31:06 +0800

    TEST: api filtering

--- client/src/pages/products/AllProducts.tsx
+++ client/src/pages/products/AllProducts.tsx
@@ -1,108 +1,68 @@
 import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
-function AllProducts() {
-
-    const cardData = [
-        {
-          id: 1,
-          img: "https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Women's Striped Shirt",
-          isNew: true,
-          oldPrice: "0",
-          currentPrice: 15,
-        },
-        {
-          id: 2,
-          img: "https://images.pexels.com/photos/1113554/pexels-photo-1113554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Women's Striped Pants",
-          isNew: true,
-          oldPrice: "0",
-          currentPrice: 30,
-        },
-        {
-          id: 3,
-          img: "https://images.pexels.com/photos/923711/pexels-photo-923711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Men's Black Leather Jacket",
-          isNew: false,
-          oldPrice: 275,
-          currentPrice: 199,
-        },
-        {
-          id: 4,
-          img: "https://images.pexels.com/photos/923711/pexels-photo-923711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Men's Black Leather Jacket",
-          isNew: false,
-          oldPrice: 32,
-          currentPrice: 20,
-        },
-        {
-          id: 5,
-          img: "https://images.pexels.com/photos/952629/pexels-photo-952629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Women's Three Piece Suit Maroon",
-          isNew: true,
-          oldPrice: "",
-          currentPrice: 299,
-        },
-        {
-          id: 6,
-          img: "https://images.pexels.com/photos/952629/pexels-photo-952629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Women's Three Piece Suit Maroon",
-          isNew: false,
-          oldPrice: 323,
-          currentPrice: 199,
-        },
-        {
-          id: 7,
-          img: "https://images.pexels.com/photos/952629/pexels-photo-952629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Women's Three Piece Suit Maroon",
-          isNew: true,
-          oldPrice: "",
-          currentPrice: 236,
-        },
-        {
-          id: 8,
-          img: "https://images.pexels.com/photos/952629/pexels-photo-952629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          title: "Women's Three Piece Suit Maroon",
-          isNew: false,
-          oldPrice: 199,
-          currentPrice: 99,
-        },
-      ];
-
+function AllProducts({
+  selectedSubCat,
+  maxPrice,
+  sort,
+  categoryId,
+}: {
+  selectedSubCat: any[];
+  maxPrice: number;
+  sort: string;
+  categoryId: any;
+}) {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&${selectedSubCat.map(
+      (data) => `&[filters][sub_categories][id][$eq]=${data}`
+    )}`
+  );
+  console.log(selectedSubCat);
   return (
     <>
-      {cardData.map((data) => (
-        <Link key={data.id} to={`/product/${data.id}`}>
-          <div
-            className="flex flex-col h-full outline-1 outline outline-gray-400"
-          >
-            <img className="w-screen object-cover" src={data.img} alt={data.title} />
-            <p
-              className="absolute px-2 py-2 text-4xl text-black font-semibold italic"
-              style={
-                data.isNew
-                  ? { display: "flex", textShadow: "white 5px 5px" }
-                  : { display: "none" }
-              }
-            >
-              New Season
-            </p>
-            <div className="flex flex-col p-3">
-              <p className="font-semibold text-xl mt-2">{data.title}</p>
-              <div className="flex flex-col">
-                <p className="text-gray-500 line-through">
-                  {data.oldPrice} <span>USD</span>
+      {error
+        ? "Something went wrong"
+        : loading
+        ? "loading"
+        : data.map((data) => (
+            <Link key={data.id} to={`/product/${data.id}`}>
+              <div className="flex flex-col h-full outline-1 outline outline-gray-400">
+                <img
+                  className="h-96 object-cover"
+                  src={
+                    "http://localhost:1337" +
+                    data.attributes.image.data.attributes.url
+                  }
+                  alt={data.attributes.title}
+                />
+                <p
+                  className="absolute px-2 py-2 text-4xl text-black font-semibold italic"
+                  style={
+                    data.attributes.isNew
+                      ? { display: "flex", textShadow: "white 5px 5px" }
+                      : { display: "none" }
+                  }
+                >
+                  New Season
                 </p>
-                <p className="font-semibold">
-                  {data.currentPrice} <span>USD</span>
-                </p>
+                <div className="flex flex-col p-3">
+                  <p className="font-semibold text-xl mt-2">
+                    {data.attributes.title}
+                  </p>
+                  <div className="flex flex-col">
+                    <p className="text-gray-500 line-through">
+                      {data.attributes.oldPrice} <span>USD</span>
+                    </p>
+                    <p className="font-semibold">
+                      {data.attributes.currentPrice} <span>USD</span>
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </Link>
-      ))}
+            </Link>
+          ))}
     </>
-  )
+  );
 }
 
-export default AllProducts
+export default AllProducts;
