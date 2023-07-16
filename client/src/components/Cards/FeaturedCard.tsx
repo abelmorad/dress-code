de55ee72cfe8d3311176fcc2b5c7b90db28e0419
abelmorad <abelmorad97@gmail.com>
2023-07-16 10:34:36 +0800

    Refactor

--- client/src/components/Cards/FeaturedCard.tsx
+++ client/src/components/Cards/FeaturedCard.tsx
@@ -1,64 +1,64 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import useFetch from "../../hooks/useFetch";
 
 function FeaturedCard() {
-  const [data, setData] = useState<any[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get("http://localhost:1337/api/products?populate=*&[filters][type][$eq]=featured", {
-          headers: {
-            Authorization: "bearer" + process.env.REACT_APP_API_TOKEN,
-          },
-        });
-        setData(res.data.data);
-        console.log(res.data.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=featured`
+  );
 
   return (
     <>
-      {data.map((data) => (
-        <Link to={`/product/${data.id}`} key={data.id} type={data.attributes.type}>
-          <div className="flex flex-col justify-between h-full outline-1 outline outline-gray-400">
-            <img className="h-96 w-screen object-cover" src={"http://localhost:1337" + data.attributes.image.data.attributes.url} />
-            <p
-              className="absolute px-2 py-2 text-4xl text-black font-semibold italic"
-              style={
-                data.attributes.isNew
-                  ? { display: "flex", textShadow: "white 5px 5px" }
-                  : { display: "none" }
-              }
+      {error
+        ? "Something went wrong!"
+        : loading
+        ? "loading"
+        : data.map((data) => (
+            <Link
+              to={`/product/${data.id}`}
+              key={data.id}
+              type={data.attributes.type}
             >
-              New Season
-            </p>
-            <div className="flex flex-col px-3">
-              <p className="font-semibold text-xl pt-3">
-                {data.attributes.title}
-              </p>
-              <div className="flex flex-col">
+              <div className="flex flex-col justify-between h-full outline-1 outline outline-gray-400">
+                <img
+                  className="h-96 w-screen object-cover"
+                  src={
+                    "http://localhost:1337" +
+                    data.attributes.image.data.attributes.url
+                  }
+                />
                 <p
-                  className="text-gray-500 line-through"
+                  className="absolute px-2 py-2 text-4xl text-black font-semibold italic"
                   style={
-                    data.attributes.oldPrice === null
-                      ? { display: "none" }
-                      : { display: "block" }
+                    data.attributes.isNew
+                      ? { display: "flex", textShadow: "white 5px 5px" }
+                      : { display: "none" }
                   }
                 >
-                  {data.attributes.oldPrice} USD
+                  New Season
+                </p>
+                <div className="flex flex-col px-3">
+                  <p className="font-semibold text-xl pt-3">
+                    {data.attributes.title}
+                  </p>
+                  <div className="flex flex-col">
+                    <p
+                      className="text-gray-500 line-through"
+                      style={
+                        data.attributes.oldPrice === null
+                          ? { display: "none" }
+                          : { display: "block" }
+                      }
+                    >
+                      {data.attributes.oldPrice} USD
+                    </p>
+                  </div>
+                </div>
+                <p className="font-semibold px-3 pb-3">
+                  {data.attributes.price} USD
                 </p>
               </div>
-            </div>
-            <p className="font-semibold px-3 pb-3">{data.attributes.price} USD</p>
-          </div>
-        </Link>
-      ))}
+            </Link>
+          ))}
     </>
   );
 }
