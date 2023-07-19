a87d22e609940bf6b43acbc6c5f2a844c2098683
abelmorad <abelmorad97@gmail.com>
2023-07-19 11:44:53 +0800

    git

--- client/src/redux/store/store.tsx
+++ client/src/redux/store/store.tsx
@@ -1,15 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../reducer/cartReducer';
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from '../reducer/cartReducer'
 
-const store = configureStore({
+export default configureStore({
   reducer: {
     cart: cartReducer
   },
 })
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
-
-export default store
