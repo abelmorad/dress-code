258c984843078d47449212a4daad00272ecc29f3
abelmorad <abelmorad97@gmail.com>
2023-07-20 08:42:13 +0800

    Style: Hero UI update

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,15 +1,15 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, EffectFade } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/effect-fade";
 
 import FeaturedSection from "../../components/Sections/FeaturedSection";
 import CategorySection from "../../components/Sections/CategorySection";
 import TrendingSection from "../../components/Sections/TrendingSection";
 
 import useFetch from "../../hooks/useFetch";
+import { Link } from "react-router-dom";
 
 function Home() {
   function HeroSlider() {
@@ -17,20 +17,19 @@ function Home() {
 
     return (
       <Swiper
-        modules={[Autoplay, Pagination, EffectFade]}
+        modules={[Autoplay]}
         slidesPerView={1}
-        autoplay
-        pagination
-        effect={"fade"}
+        autoplay={true}
       >
         {error
           ? "Something went wrong"
           : loading
           ? "loading"
           : data.map((data) => (
-              <SwiperSlide className="flex place-content-center" key={data.id}>
+              <SwiperSlide key={data.id}>
                 <img
-                  className="w-screen h-screen object-cover"
+                  style={{ height: "600px", width: "600px" }}
+                  className="object-cover rounded-2xl"
                   src={
                     "http://localhost:1337" +
                     data.attributes.image.data.attributes.url
@@ -44,7 +43,19 @@ function Home() {
   }
   return (
     <section className="flex flex-col">
-      <section className="flex mb-20">
+      <section className="grid grid-flow-col my-24 grid-cols-2 overflow-hidden">
+        <div className="flex flex-col place-content-center pl-28">
+          <h1 className="text-7xl font-extrabold leading-snug mb-3">
+            Look Good, Feel Good, Dress Good,{" "}
+            <span className="rainbow-text">Dress Code</span>
+          </h1>
+          <Link
+            className="w-fit border-black border-2 rounded-md py-2 px-8 font-medium"
+            to="/products/1"
+          >
+            Start Shopping
+          </Link>
+        </div>
         <HeroSlider />
       </section>
       <section className="flex flex-col mb-20 mx-36">
