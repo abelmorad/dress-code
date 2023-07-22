290cb36e8e1e6f09a886a11d78fa48f43a65e27c
abelmorad <abelmorad97@gmail.com>
2023-07-22 08:41:06 +0800

    Feat: now cart closes when clicking outside of it

--- client/src/components/Sections/Navbar.tsx
+++ client/src/components/Sections/Navbar.tsx
@@ -7,15 +7,28 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import Cart from "./Cart";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 
 function Navbar() {
-  const items = useSelector((state:any) => state.cart.products);
-  const [openCart, setOpenCart] = useState(false);
-  
+  const items = useSelector((state: any) => state.cart.products);
+  const [open, setOpen] = useState(false);
+
+  const openRef = useRef<any>();
+
+  useEffect(() => {
+    const handleOpen = (e: any) => {
+      if (!openRef.current.contains(e.target)) {
+        setOpen(false);
+        console.log(openRef.current);
+      }
+    };
+
+    document.addEventListener("click", handleOpen);
+  }, []);
+
   return (
     <nav className="grid grid-cols-3 px-10 py-3 items-center fixed w-screen bg-white z-10">
       <section className="flex gap-4">
@@ -61,19 +74,19 @@ function Navbar() {
           <SearchOutlinedIcon />
           <PersonOutlineOutlinedIcon />
           <FavoriteBorderOutlinedIcon />
-          <div className="flex" onClick={() => setOpenCart(!openCart)}>
-            <ShoppingCartOutlinedIcon />
-            <span className="flex text-sm bg-blue-500 h-5 w-5 items-center place-content-center rounded-full absolute ml-3.5 top-0 mt-2 text-white font-semibold">
-              {items.length}
-            </span>
+          <div className="flex" ref={openRef}>
+            <div className="flex" onClick={() => setOpen(!open)}>
+              <ShoppingCartOutlinedIcon />
+              <span className="flex text-sm bg-blue-500 h-5 w-5 items-center place-content-center rounded-full absolute ml-3.5 top-0 mt-2 text-white font-semibold">
+                {items.length}
+              </span>
+            </div>
+            <section style={open ? { display: "block" } : { display: "none" }}>
+              <Cart />
+            </section>
           </div>
         </div>
       </section>
-      {openCart && (
-        <section>
-          <Cart />
-        </section>
-      )}
     </nav>
   );
 }
